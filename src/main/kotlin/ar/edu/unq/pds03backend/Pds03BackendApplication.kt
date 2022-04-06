2cbdb16653e99308a18d6dfc5324ac3df47bfd45
Jose Cassano <joseluis.cassano@fravega.com.ar>
2022-04-06 16:33:25 -0300

    Set up

---
+++ src/main/kotlin/ar/edu/unq/pds03backend/Pds03BackendApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.pds03backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Pds03BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<Pds03BackendApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/pds03backend/Pds03BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.pds03backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Pds03BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
