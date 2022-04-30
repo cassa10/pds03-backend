3b07a5219ae6c8f0da716d1a8c45de9abd0fbe1f
Matías Cabrera <mcabrera@cys.com.ar>
2022-04-30 17:08:46 -0300

    Semester, Career and Subject mapped

--- src/main/kotlin/ar/edu/unq/pds03backend/model/Career.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/model/Career.kt
@@ -1,6 +1,18 @@
 package ar.edu.unq.pds03backend.model
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "careers")
 class Career(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
-    val name: String
+    @Column(unique = true, nullable = false)
+    val name: String,
+    @ManyToMany(cascade = arrayOf(CascadeType.ALL))
+    @JoinTable(name = "career_subject",
+        joinColumns = arrayOf(JoinColumn(name = "career_id", referencedColumnName = "id")),
+        inverseJoinColumns = arrayOf(JoinColumn(name = "subject_id", referencedColumnName = "id")))
+    val subjects: Collection<Subject>
 )
