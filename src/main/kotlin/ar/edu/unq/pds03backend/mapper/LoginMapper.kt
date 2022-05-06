dd2e7598d7d6f02d36dbf12f3d876b7279c1a5f2
Matías Cabrera <mcabrera@cys.com.ar>
2022-05-06 15:35:56 -0300

    Service annotation removed

--- src/main/kotlin/ar/edu/unq/pds03backend/mapper/LoginMapper.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/mapper/LoginMapper.kt
@@ -5,7 +5,6 @@ import ar.edu.unq.pds03backend.dto.LoginResponseDTO
 import ar.edu.unq.pds03backend.model.Role
 import org.springframework.stereotype.Service
 
-@Service
 class LoginMapper : Mapper<User, LoginResponseDTO> {
 
     override fun toDTO(user: User): LoginResponseDTO {
--- src/main/kotlin/ar/edu/unq/pds03backend/service/impl/AuthService.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/service/impl/AuthService.kt
@@ -11,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class AuthService(@Autowired private val personService: PersonService, @Autowired private val loginMapper: LoginMapper) : IAuthService {
+class AuthService(@Autowired private val personService: PersonService) : IAuthService {
     override fun login(loginRequestDTO: LoginRequestDTO): LoginResponseDTO {
         val person = validate(loginRequestDTO)
 
         // TODO: Crear JWT
 
-        return loginMapper.toDTO(person.user)
+        return LoginMapper().toDTO(person.user)
     }
 
     private fun validate(loginRequestDTO: LoginRequestDTO): Person {
