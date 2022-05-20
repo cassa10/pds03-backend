342aab9631b810791b1372ba92e2b9d24bbfad76
Jose Cassano <joseluis.cassano@fravega.com.ar>
2022-05-20 18:56:37 -0300

    Add requested and accepted quotes in course DTO, and add calculations of these fields in quoteRequestRepo

--- src/main/kotlin/ar/edu/unq/pds03backend/dto/quoteRequest/QuoteRequestResponseDTO.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/dto/quoteRequest/QuoteRequestResponseDTO.kt
@@ -1,12 +1,12 @@
 package ar.edu.unq.pds03backend.dto.quoteRequest
 
-import ar.edu.unq.pds03backend.dto.course.CourseResponseDTO
+import ar.edu.unq.pds03backend.dto.course.SimpleCourseResponseDTO
 import ar.edu.unq.pds03backend.dto.student.StudentResponseDTO
 import ar.edu.unq.pds03backend.model.QuoteState
 
 data class QuoteRequestResponseDTO(
     val id: Long,
-    val course: CourseResponseDTO,
+    val course: SimpleCourseResponseDTO,
     val student: StudentResponseDTO,
     val state: QuoteState,
     val comment: String
--- src/main/kotlin/ar/edu/unq/pds03backend/repository/IQuoteRequestRepository.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/repository/IQuoteRequestRepository.kt
@@ -1,6 +1,7 @@
 package ar.edu.unq.pds03backend.repository
 
 import ar.edu.unq.pds03backend.model.QuoteRequest
+import ar.edu.unq.pds03backend.model.QuoteState
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import java.util.*
@@ -10,4 +11,5 @@ interface IQuoteRequestRepository : JpaRepository<QuoteRequest, Long> {
     fun findAllByCourseIdAndStudentId(idCourse: Long, idStudent: Long): Iterable<QuoteRequest>
     fun findAllByCourseId(idCourse: Long): Iterable<QuoteRequest>
     fun findAllByStudentId(idStudent: Long): Iterable<QuoteRequest>
+    fun countByStateAndCourseId(quoteState: QuoteState, courseId: Long): Int
 }
