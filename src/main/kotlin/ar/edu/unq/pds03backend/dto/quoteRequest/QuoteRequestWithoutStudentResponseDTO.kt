bc895a659a6403bd7b8cdbb831ff574ec2432336
Matías Cabrera <mcabrera@cys.com.ar>
2022-05-23 21:36:52 -0300

    Add info subject to response

--- src/main/kotlin/ar/edu/unq/pds03backend/dto/quoteRequest/QuoteRequestWithoutStudentResponseDTO.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/dto/quoteRequest/QuoteRequestWithoutStudentResponseDTO.kt
@@ -1,11 +1,13 @@
 package ar.edu.unq.pds03backend.dto.quoteRequest
 
 import ar.edu.unq.pds03backend.dto.course.SimpleCourseForSubjectDTO
+import ar.edu.unq.pds03backend.dto.subject.SimpleSubjectResponseDTO
 import ar.edu.unq.pds03backend.model.QuoteState
 
 data class QuoteRequestWithoutStudentResponseDTO(
     val id: Long,
     val course: SimpleCourseForSubjectDTO,
+    val subject: SimpleSubjectResponseDTO,
     val state: QuoteState,
     val comment: String,
     val adminComment: String
