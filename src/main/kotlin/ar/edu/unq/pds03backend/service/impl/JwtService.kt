75c2d8ac0a1e52fc553d96588501f659d768c0ef
Dani <43595337+DaniVillegas14@users.noreply.github.com>
2022-07-12 00:27:08 -0300

    [Feature] Sec endpoints director (#66)

--- src/main/kotlin/ar/edu/unq/pds03backend/service/impl/JwtService.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/service/impl/JwtService.kt
@@ -1,11 +1,15 @@
 package ar.edu.unq.pds03backend.service.impl
 
+import ar.edu.unq.pds03backend.exception.UserNotFoundException
 import ar.edu.unq.pds03backend.model.User
+import ar.edu.unq.pds03backend.repository.IUserRepository
 import ar.edu.unq.pds03backend.service.IJwtService
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.io.Serializable
 import java.util.*
@@ -13,7 +17,8 @@ import java.util.*
 @Component
 class JwtService(
     @Value("\${jwt.secret}")
-    private val secret: String? = null
+    private val secret: String? = null,
+    @Autowired private val userRepository: IUserRepository
 ): IJwtService, Serializable {
 
     companion object {
@@ -35,6 +40,13 @@ class JwtService(
         return getClaimFromToken(token) { it.issuer }
     }
 
+    fun getDniFromToken(token: String): String {
+        val id = getUserIdFromToken(token)
+        val user = userRepository.findById(id.toLong())
+        if (!user.isPresent) throw UserNotFoundException()
+
+        return user.get().dni
+    }
     override fun getExpirationDateFromToken(token: String): Date {
         return getClaimFromToken(token) { it.expiration }
     }
@@ -46,6 +58,11 @@ class JwtService(
     override fun isTokenExpired(token: String): Boolean {
         val expiration: Date = getExpirationDateFromToken(token)
         return expiration.before(Date())
+    }
+
+    fun validateToken(token: String, userDetails: UserDetails): Boolean {
+        val username: String = getDniFromToken(token)
+        return username == userDetails.username && !isTokenExpired(token)
     }
 
     private fun doGenerateToken(userId: String): String {
