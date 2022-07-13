8df08939222af40ad38bdf2878c6a2f1c4652e9f
Dani Villegas <dani.villegascarp@gmail.com>
2022-07-12 21:16:41 -0300

    fix sec

--- src/main/kotlin/ar/edu/unq/pds03backend/config/AppConfig.kt
+++ src/main/kotlin/ar/edu/unq/pds03backend/config/AppConfig.kt
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.PathSelectors
@@ -16,6 +13,11 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.concurrent.Executor
+import springfox.documentation.service.ApiKey
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.AuthorizationScope
+import com.google.common.collect.Lists
 
 @Configuration
 @EnableSwagger2
@@ -41,10 +43,32 @@ class AppConfig(
 
     @Bean
     fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .securityContexts(Lists.newArrayList(securityContext()))
+        .securitySchemes(Lists.newArrayList(apiKey()))
         .select()
         .apis(RequestHandlerSelectors.basePackage("ar.edu.unq.pds03backend.api"))
         .paths(PathSelectors.any())
         .build()
+
+
+    private fun apiKey(): ApiKey {
+        return ApiKey("JWT","Authorization", "header")
+    }
+
+    private fun securityContext(): SecurityContext {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build()
+    }
+
+    fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls(1)
+        authorizationScopes[0] = authorizationScope
+        return Lists.newArrayList(
+                SecurityReference("JWT", authorizationScopes))
+    }
 
     @Bean
     fun forwardToIndex(): WebMvcConfigurer {
